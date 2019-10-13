95250aac24ecd84057bdb6e8af6fa2e8d1b7ce8e
teanzhengyang <teanzhengyang@outlook.sg>
2019-10-13 18:06:21 +0800

    install kubenetes 1.15 for dashboard 2.0.0 compatibility

--- k8dashboard/adddashboard.sh
+++ k8dashboard/adddashboard.sh
@@ -1,3 +1,3 @@
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta4/aio/deploy/recommended.yaml
 kubectl apply -f dashboard-adminuser.yaml
 kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
